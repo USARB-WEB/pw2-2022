b40fb46094bb4c66118098060160b166efee8720
sergiuchilat <chilatsergiu@gmail.com>
2022-09-07 13:46:41 +0300

    Add 2 modules: Regions and Locations

--- examples/express/src/routes/index.js
+++ examples/express/src/routes/index.js
@@ -1,7 +1,7 @@
 const Router = require('express')
 const router = new Router()
 
-const modules = ['countries']
+const modules = ['countries', 'regions', 'locations']
 
 modules.forEach(module => {
     router.use(`/${module}`, require(`./${module}`))
