5c7453ad4906e7d989385b810e2dc84016eec67e
sergiuchilat <chilatsergiu@gmail.com>
2022-09-07 12:51:44 +0300

    Create module with routes and controllers

--- examples/express/src/app.js
+++ examples/express/src/app.js
@@ -4,39 +4,8 @@ const port = 3000
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-    response.send('Main page')
-})
-
-app.get('/countries', (request, response) => {
-    response.header("Content-Type", "application/json");
-    response.send([
-        {
-            id: 1,
-            name: "Moldova",
-            code: "MD",
-            flag: null,
-            area: 1400000,
-            population: 2890321
-        }
-    ])
-})
-
-app.post('/countries', (request, response) => {
-    const country = request.body;
-    country.id = 1; 
-    response.json(country);
-})
-
-app.patch('/countries/:id', (request, response) => {
-    const country = request.body;
-    country.id = request.params.id;
-    response.json(country);
-})
-
-app.delete('/countries/:id', (request, response) => {
-    response.json({});
-});
+const router = require("./routes/index");
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
